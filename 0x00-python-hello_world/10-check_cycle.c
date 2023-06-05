f661fd4b6e1a9bb57eb5ebe7411a6c236fe56edd
Menna Hassan <h.menna85@gmail.com>
2023-06-05 16:35:41 -0700

    10. Linked list cycle

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * check_cyle - a fun to check
+ * @list: list to be checked
+ * Return: 0 if there's no cycle
+ */
+int check_cycle(listint_t *list)
+{
+listint_t *head, *current;
+head = list;
+current = head;
+while(current != NULL)
+{
+current = current->next;
+if (current == head)
+	return (1);
+}
+return (0);
+}
