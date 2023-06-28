0aab8ee823447118e3d3b42ce24e98438aa1d98a
Menna Hassan <h.menna85@gmail.com>
2023-06-27 17:53:00 -0700

    updated

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""new class"""
+
+
+class Square:
+    """a class to define a square
+    """
+
+    def __init__(self, size=0):
+        """initialize attributes
+        size: private inst attribute
+        """
+        self.__size = size
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        """a method to return the area"""
+        return self.__size ** 2
