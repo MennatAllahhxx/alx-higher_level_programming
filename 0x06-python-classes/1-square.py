57228e8e0d100116b44c26ddd645ba3ede44e7b9
Menna Hassan <h.menna85@gmail.com>
2023-06-27 17:40:43 -0700

    updated

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""new class"""
+
+
+class Square:
+    """a class to define a square
+    """
+
+    def __init__(self, size=0):
+        """initialize attributes
+        size: private inst attribute
+        """
+        self.__size = size
