6d686df776fdbefb82266f909cc01d343686e72e
Menna Hassan <h.menna85@gmail.com>
2023-07-10 13:12:06 -0700

    updated

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+this is '6-base_geometry' module
+
+this module contains 1 class: BaseGeometry
+
+BaseGeometry contains 1 method: area(self)
+"""
+
+
+class BaseGeometry:
+    """
+    this is a class to do some geometry
+    """
+    def area(self):
+        """
+        a method to calculate the area
+        :return: area
+        """
+        raise Exception("area() is not implemented")
