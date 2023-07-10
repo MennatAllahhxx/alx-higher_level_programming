94a05157ae0dd778cdf8e49fe5b84230876fbaa6
Menna Hassan <h.menna85@gmail.com>
2023-07-10 16:06:10 -0700

    updated

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -2,37 +2,13 @@
 """
 this is '8-rectangle' module
 
-this module contains 2 classes: BaseGeometry, Rectangle
+this module contains 1 class: Rectangle
 
-BaseGeometry contains 2 methods: area(self),
-integer_validator(self, name, value)
+importing BaseGeometry
 """
 
 
-class BaseGeometry:
-    """
-    this is a class to do some geometry
-    """
-
-    def area(self):
-        """
-        a method to calculate the area
-        :return: area
-        """
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """
-        a method to validate integer
-        :param name: name of object
-        :param value: its value
-        :return: nth probably
-        """
-
-        if type(value) is not int:
-            raise TypeError(f"{name} must be an integer")
-        if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
 
 
 class Rectangle(BaseGeometry):
