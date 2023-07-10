5df39941471b8925855782b4498f099ddd966100
Menna Hassan <h.menna85@gmail.com>
2023-07-10 13:03:22 -0700

    updated

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+this is '5-base_geometry' module
+
+this module contains 1 class: BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """
+    this is a class to do some geometry
+    """
+    pass
