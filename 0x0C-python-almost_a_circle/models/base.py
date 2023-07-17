240ed40cb1d2fd93cb5c0da567c154ffb330b7c0
Menna Hassan <h.menna85@gmail.com>
2023-07-17 13:13:08 -0700

    updated

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -3,7 +3,11 @@
 this is 'base' module
 
 this module contains 1 class: Base
+
+this class contains 5 methods: to_json_string, from_json_string, create,
+                                save_to_file, load_from_file
 """
+import json
 
 
 class Base:
@@ -23,3 +27,71 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = self.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """
+        method to convert dict to json
+        :return: json-string
+        """
+        if list_dictionaries is None:
+            return []
+        return json.dumps(list_dictionaries)
+
+    @staticmethod
+    def from_json_string(json_string):
+        """
+        method to convert json to dict
+        :return: dictionary
+        """
+        if json_string is None:
+            return []
+        return json.loads(json_string)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """
+        a method to write json repr to file
+        :param list_objs: list of json obj
+        :return: file
+        """
+        file = cls.__name__ + ".json"
+        my_list = []
+        with open(file, 'w', encoding="utf-8") as file:
+            if list_objs is None:
+                file.write(cls.to_json_string([]))
+            else:
+                for i in list_objs:
+                    my_list.append(cls.to_dictionary(i))
+                file.write(cls.to_json_string(my_list))
+
+    @classmethod
+    def create(cls, **dictionary):
+        """
+        method to create obj ig
+        :param dictionary: dict to create obj from
+        :return: instance
+        """
+        if "Rectangle" == cls.__name__:
+            dummy = cls(3, 3)
+        if "Square" == cls.__name__:
+            dummy = cls(3)
+        dummy.update(**dictionary)
+        return dummy
+
+    @classmethod
+    def load_from_file(cls):
+        """
+        load lists from file
+        :return: list of instances
+        """
+        file = cls.__name__ + '.json'
+        my_list = []
+        try:
+            with open(file, 'r', encoding="utf-8") as file:
+                inst = cls.from_json_string(file.read())
+                for i in inst:
+                    my_list.append(cls.create(**i))
+        except:
+            pass
+        return my_list
---
+++ 0x0C-python-almost_a_circle/tests/test_rectangle.py
@@ -0,0 +1,192 @@
+#!/usr/bin/python3
+"""
+this is 'test_rectangle' module
+
+unittest for 'rectangle' module
+"""
+import inspect
+import io
+import unittest
+from io import StringIO
+from models.rectangle import Rectangle
+from models.base import Base
+import pep8
+
+
+class TestRectangle(unittest.TestCase):
+    """
+    a class to test Rectangle class and its methods
+    """
+
+    @classmethod
+    def get_methods(cls):
+        """
+        a method to get the methods in the class
+        :return: nth
+        """
+        cls.methods = inspect.getmembers(Rectangle, inspect.isfunction)
+
+    def test_module_doctest(self):
+        """
+        test to check doctest of module
+        :return: nth
+        """
+        self.assertTrue(len(rectangle.__doc__) > 1)
+
+    def test_class_doctest(self):
+        """
+        test to check doctest of class
+        :return: nth
+        """
+        self.assertTrue(len(Rectangle.__doc__) > 1)
+
+    def test_methods_doctest(self):
+        """
+        test to check doctest of methods
+        :return: nth
+        """
+        for method in self.methods:
+            self.assertTrue(len(method[1].__doc__) > 1)
+
+    def test_class_pep8(self):
+        """
+        test for pep8 of 'rectangle' module
+        :return: nth
+        """
+        pep8style = pep8.StyleGuide(quiet=True)
+        result = pep8style.check_files('models/rectangle')
+        self.assertEqual(result.total_errors, 0, "PEP8 errors")
+
+    def test_task2(self):
+        """
+        test for task 2
+        :return: nth
+        """
+        r1 = Rectangle(10, 2)
+        r2 = Rectangle(2, 10)
+        r3 = Rectangle(10, 2, 0, 0, 12)
+        self.assertEqual(r1.id, 1)
+        self.assertEqual(r2.id, 2)
+        self.assertEqual(r3.id, 12)
+
+    def test_task3(self):
+        """
+        test for task 3
+        :return: nth
+        """
+        with self.assertRaises(TypeError):
+            Rectangle(10, "2")
+        with self.assertRaises(ValueError):
+            r = Rectangle(10, 2)
+            r.width = -10
+        with self.assertRaises(TypeError):
+            r = Rectangle(10, 2)
+            r.x = {}
+        with self.assertRaises(ValueError):
+            Rectangle(10, 2, 3, -1)
+
+    def test_task4(self):
+        """
+        test for task 4
+        :return: nth
+        """
+        self.assertEqual(Rectangle(3, 2).area, 6)
+        self.assertEqual(Rectangle(2, 10).area, 20)
+        self.assertEqual(Rectangle(8, 7, 0, 0, 12).area, 56)
+
+    def test_task5(self):
+        """
+        test for task 5
+        :return: nth
+        """
+        r1 = Rectangle(4, 6)
+        with io.StringIO() as buff, redirect_stdout(buff):
+            r1.display()
+            actual_op = buff.getvalue()
+            expected_op = "####\n####\n####\n####\n####\n####\n"
+            self.assertEqual(actual_op, expected_op)
+        r1 = Rectangle(2, 2)
+        with io.StringIO() as buff, redirect_stdout(buff):
+            r1.display()
+            actual_op = buff.getvalue()
+            expected_op = "##\n##\n"
+            self.assertEqual(actual_op, expected_op)
+
+    def test_task6(self):
+        """
+        test for task 6
+        :return: nth
+        """
+        r1 = Rectangle(4, 6, 2, 1, 12)
+        self.assertEqual(str(r1), "[Rectangle] (12) 2/1 - 4/6")
+        r2 = Rectangle(5, 5, 1)
+        self.assertEqual(str(r2), "[Rectangle] (1) 1/0 - 5/5")
+
+    def test_task7(self):
+        """
+        test for task 7
+        :return: nth
+        """
+        r1 = Rectangle(2, 3, 2, 2)
+        with io.StringIO() as buff, redirect_stdout(buff):
+            r1.display()
+            actual_op = buff.getvalue()
+            expected_op = "\n\n  ##\n  ##\n  ##\n"
+            self.assertEqual(actual_op, expected_op)
+        r1 = Rectangle(3, 2, 1, 0)
+        with io.StringIO() as buff, redirect_stdout(buff):
+            r1.display()
+            actual_op = buff.getvalue()
+            expected_op = " ###\n ###\n"
+            self.assertEqual(actual_op, expected_op)
+
+    def test_task8(self):
+        """
+        test for task 8
+        :return: nth
+        """
+        r1 = Rectangle(10, 10, 10, 10)
+        self.assertEqual(str(r1), "[Rectangle] (1) 10/10 - 10/10")
+        r1.update(89)
+        self.assertEqual(str(r1), "[Rectangle] (89) 10/10 - 10/10")
+        r1.update(89, 2)
+        self.assertEqual(str(r1), "[Rectangle] (89) 10/10 - 2/10")
+        r1.update(89, 2, 3)
+        self.assertEqual(str(r1), "[Rectangle] (89) 10/10 - 2/3")
+        r1.update(89, 2, 3, 4)
+        self.assertEqual(str(r1), "[Rectangle] (89) 4/10 - 2/3")
+        r1.update(89, 2, 3, 4, 5)
+        self.assertEqual(str(r1), "[Rectangle] (89) 4/5- 2/3")
+
+    def test_task9(self):
+        """
+        test for task 9
+        :return: nth
+        """
+        r1 = Rectangle(10, 10, 10, 10)
+        self.assertEqual(str(r1), "[Rectangle] (1) 10/10 - 10/10")
+        r1.update(height=1)
+        self.assertEqual(str(r1), "[Rectangle] (1) 10/10 - 10/1")
+        r1.update(width=1, x=2)
+        self.assertEqual(str(r1), "[Rectangle] (1) 2/10 - 1/1")
+        r1.update(y=1, width=2, x=3, id=89)
+        self.assertEqual(str(r1), "[Rectangle] (89) 3/1 - 2/1")
+        r1.update(x=1, height=2, y=3, width=4)
+        self.assertEqual(str(r1), "[Rectangle] (89) 1/3 - 4/2")
+
+    def test_task13(self):
+        """
+        test for task 13
+        :return: nth
+        """
+        r1 = Rectangle(10, 2, 1, 9)
+        self.assertEqual(str(r1), "[Rectangle] (1) 1/9 - 10/2")
+        r1_dictionary = r1.to_dictionary()
+        self.assertEqual(r1_dictionary,
+                         {'x': 1, 'y': 9, 'id': 1, 'height': 2, 'width': 10})
+        self.assertEqual(type(r1_dictionary), dict)
+        r2 = Rectangle(1, 1)
+        self.assertEqual(str(r2), "[Rectangle] (2) 0/0 - 1/1")
+        r2.update(**r1_dictionary)
+        self.assertEqual(str(r2), "[Rectangle] (1) 1/9 - 10/2")
+        self.assertFalse(r1 == r2)
