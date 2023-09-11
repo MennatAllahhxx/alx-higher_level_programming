4613f9fcf7aaeeab6e097460038c7f9f6f0aa2eb
Menna Hassan <h.menna85@gmail.com>
2023-09-11 14:00:26 -0700

    updated

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -4,7 +4,7 @@ const argvb = process.argv[3];
 const a = Number(argva);
 const b = Number(argvb);
 const result = add(a, b);
-function add(a, b) {
+function add (a, b) {
   return (a + b);
 }
 console.log(result);
