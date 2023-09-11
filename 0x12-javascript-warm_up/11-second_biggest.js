1e3dc9dba508ef02b20a5c0d48ff593a229ccf86
Menna Hassan <h.menna85@gmail.com>
2023-09-11 16:01:32 -0700

    updated

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,7 +1,5 @@
 #!/usr/bin/node
-const myArgs = process.argv.slice(2).map(argv => Number(argv));
-myArgs.sort();
-myArgs.reverse();
+const myArgs = process.argv.slice(2).map(Number).sort((a, b) => b - a);
 if (isNaN(myArgs[1]) || myArgs[1] === undefined) {
   console.log(0);
 } else {
