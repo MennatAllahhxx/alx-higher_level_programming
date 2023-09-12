780a5574fe188e0d3c356361e60527eb83e22004
Menna Hassan <h.menna85@gmail.com>
2023-09-13 00:49:29 +0200

    updated

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (typeof w === 'number' && typeof h === 'number' && w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+  print() {
+    for (let index = 0; index < this.height; index++) {
+      let str = 'X';
+      console.log(str.repeat(this.width));
+    }
+  }
+}
+module.exports = Rectangle;
