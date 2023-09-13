04394a06de0f0d65c3400665de02453db338f4d8
Menna Hassan <h.menna85@gmail.com>
2023-09-12 19:46:53 -0700

    updated

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+const dict = require('./101-data');
+
+const userIdsByOccurrence = {};
+
+for (const userId in dict) {
+  const occurrence = dict[userId];
+  if (!userIdsByOccurrence.hasOwnProperty(occurrence)) {
+    userIdsByOccurrence[occurrence] = [];
+  }
+  userIdsByOccurrence[occurrence].push(userId);
+}
+
+console.log(userIdsByOccurrence);
