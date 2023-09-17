733fac478ef57b083a44fe8c99e62b2df6879d35
Menna Hassan <h.menna85@gmail.com>
2023-09-17 19:23:20 +0300

    updated

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+import MySQLdb
+from sys import argv
+
+
+def get_database():
+    """
+    a function to get the database
+    """
+
+    db = MySQLdb.connect(
+        host='localhost',
+        port=3306,
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3]
+    )
+
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+    rows = cursor.fetchall()
+
+    for row in rows:
+        print(row)
+
+    cursor.close()
+    db.close()
+
+
+if __name__ == '__main__':
+    get_database()
