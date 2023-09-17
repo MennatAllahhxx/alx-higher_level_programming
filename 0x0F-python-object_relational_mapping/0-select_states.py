9ad19446191d7f2ca822b74eb3a151977ad8bc02
Menna Hassan <h.menna85@gmail.com>
2023-09-17 19:13:45 +0300

    updated

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -9,10 +9,10 @@ def get_database():
     """
     
     db = MySQLdb.connect(
-        host="localhost",
+        host='localhost',
         port=3306,
-        username=argv[1],
-        pswd=argv[2],
+        user=argv[1],
+        passwd=argv[2],
         db=argv[3]
     )
     
@@ -27,7 +27,5 @@ def get_database():
     db.close()
     
 
-if __name__=="__main__":
+if __name__=='__main__':
     get_database()
-    
-    
