427d1f1e4fd53b54c5399c037c7fb7ba18af66d3
Menna Hassan <h.menna85@gmail.com>
2023-09-17 22:57:47 +0300

    updated

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""
+a script that lists all cities from the database hbtn_0e_4_usa
+"""
+import MySQLdb
+from sys import argv
+
+
+def get_database():
+    """
+    a function to get the database
+    """
+
+    db = MySQLdb.connect(
+        host='localhost',
+        port=3306,
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3]
+    )
+
+    cursor = db.cursor()
+    cursor.execute(f"\
+                   SELECT cities.name\
+                   FROM cities \
+                   INNER JOIN states\
+                   ON cities.state_id = states.id\
+                   WHERE name='{:s}'\ 
+                   ORDER BY cities.id ASC\
+                   ".format(argv[4]))
+    rows = cursor.fetchall()
+
+    for row in rows:
+        print(row)
+
+    cursor.close()
+    db.close()
+
+
+if __name__ == '__main__':
+    get_database()
