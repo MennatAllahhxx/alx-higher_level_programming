278eb7ff88357658b751af779a040eb9edbf5ad7
Menna Hassan <h.menna85@gmail.com>
2023-09-29 05:51:17 +0300

    updated

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #  a Bash script that sends a POST request to the passed URL
-curl -sX POST "email: test@gmail.com" -X "subject: I will always be here for PLD" "$1"
+curl -sd "email: test@gmail.com" -d "subject: I will always be here for PLD" "$1"
