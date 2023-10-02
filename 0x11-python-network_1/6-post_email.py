f1a91b625afe7e126b938c1e27e2046f0c8a2d2c
Menna Hassan <h.menna85@gmail.com>
2023-10-02 15:53:39 +0300

    updated

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""6-post_email Module"""
+
+import requests
+from urllib.request import urlopen, Request
+from urllib.parse import urlencode
+from sys import argv
+
+
+def post_email():
+    """a script to post email"""
+
+    data = {'email': argv[2]}
+    req = requests.post(argv[1], data)
+    print(req.text)
+
+
+if __name__ == '__main__':
+    post_email()
