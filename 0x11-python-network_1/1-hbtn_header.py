ec0c9f5e23a14e46e1875371340bb083b1d21436
Menna Hassan <h.menna85@gmail.com>
2023-10-02 14:47:37 +0300

    updated

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""1-hbtn_header.py Module"""
+
+from urllib.request import urlopen
+from sys import argv
+
+
+def fetch_item_from_url():
+    """a script to fetch item"""
+
+    with urlopen(argv[1]) as url_response:
+        print(url_response.getheader("X-Request-Id"))
+
+
+if __name__ == '__main__':
+    fetch_item_from_url()
