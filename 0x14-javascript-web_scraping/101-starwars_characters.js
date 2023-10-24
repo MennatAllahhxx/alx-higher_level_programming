6af945f117918f0d57499d81c480a065c8352da3
Menna Hassan <h.menna85@gmail.com>
2023-10-25 01:21:30 +0300

    updated

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,18 +1,20 @@
 #!/usr/bin/node
 const request = require('request');
 const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
-request(url, function (err, res, body) {
+request(url, async function (err, res, body) {
   if (err) {
     console.log(err);
   } else {
     const chars = JSON.parse(body).characters;
     for (const char of chars) {
-      request(char, function (err, res, body) {
-        if (err) {
-          console.log(err);
-        } else {
+      await new Promise((resolve, reject) => {
+        request(char, function (err, res, body) {
+          if (err) {
+            reject(err);
+          }
           console.log(JSON.parse(body).name);
-        }
+          resolve();
+        });
       });
     }
   }
